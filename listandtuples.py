c44b42a7b98a12691ad7c2e0a1bfc068034da4fc
Github Practice <bharatccnt@gmail.com>
2026-04-23 01:16:57 +0545

    added list

---
+++ listandtuples.py
@@ -0,0 +1,39 @@
+# # # marks1 = 94.4
+# # # marks2 = 88.5
+# # # marks3 = 78.9
+# # # marks4 = 67.5
+# # # marks5 = 56.7
+
+# # marks = [94.4, 88.5, 78.9, 67.5, 56.7] # List
+# # print(marks)
+# # print(type(marks))
+# # print(len(marks))
+
+# # Values = ["bharat", "roka", 21, "BSC.csit"]
+# # print(Values)
+# # print(Values[0])
+# # Values[0] = 9
+# # print(Values)
+# # print(Values[1:2])
+
+# #LIST METHODs..............
+
+# values = ["bharat", "roka", 21, "BSC.csit"]
+# values.append(9)
+# print(values)
+
+list = [1 ,5 ,2 ,7]
+list.sort()
+print(list)
+list.append(5)
+print(list)
+list.sort()
+print(list)
+list.insert(4,0)
+print(list)
+list.insert(0, "bharat")
+print(list)
+list.remove(0)
+print(list)
+list.pop(0)
+print(list)

ac4e3fe0196e9bbb86936b2682b84a589ac55fa4
Github Practice <bharatccnt@gmail.com>
2026-04-23 00:41:55 +0545

    day 2

---
+++ ifelifelse.py
@@ -0,0 +1,103 @@
+# # # light = input("light :")
+# # # if(light == "red"):
+# # #     print("STOP")
+# # # elif(light == "yellow"):
+# # #     print("READY")
+# # # elif(light == "green"):
+# # #     print("GO")
+# # # else:
+# # #     Print("Please enter correct color for traffic light")
+
+
+
+# # marks = int(input("Enter the student marks: "))
+# # if(marks >100):
+# #     print("Please enter the valid marks.")
+
+# # elif(marks >= 90):
+# #     print("A+")
+# # elif(marks >= 80 and marks < 90):
+# #     print("A")
+# # elif(marks >= 70 and marks < 80):
+# #     print("B+")
+# # elif(marks >= 60 and marks < 70):
+# #     print("B")
+# # elif(marks >= 50 and marks < 60):
+# #     print("C+")
+# # elif(marks >= 40 and marks < 50):
+# #     print("C")
+# # else:
+# #     print("FAILED")
+
+# # Food = input ("Food : ")
+
+# # eat ="yes" if(Food == "Cake") else "no"
+# # print(eat)
+
+
+# #type casting
+
+# # a=4
+# # b=2.5
+# # print(a+b)   Automatically conversion
+
+# # a = "2"
+
+# # b = "cat"
+
+# # print (a+b)   Manually Conversion
+
+# #WAP to find average of any flotaing numbers..........
+
+# first = float(input("Enter first number :"))
+# second = float(input("Enter second number :"))
+# average = (first + second)/2
+# print(average)
+
+# a = "Bharat_Roka"
+
+# print(a[3])
+
+# str = "i am a coder."
+# print(str.endswith("er."))
+# print(str.capitalize())
+# print(str.replace("o", "a"))
+# print(str.find("f"))
+# print(str.count("b"))
+
+#QUESTIONS
+
+# name=input("Enter your name to check length: ")
+# # print(len(name))
+
+#TO CHECK GIVE NUMBER IS ODD OR EVEN
+
+# a = float(input("enter the number: "))
+
+# if(a % 2 ==0):
+#     print("EVEN")
+# else:
+#     print("ODD")
+
+#TO CHECK THE GREATEST NUMBER
+
+# a = float(input("Enter the number: "))
+# b = float(input("Enter the number: "))
+# c = float(input("Enter the number: "))
+# if (a==b==c):
+#     print("All are same.")
+# elif(a>=b and a>=c):
+#     print("a is greater.")
+# elif(b>=c):
+#     print("b is a greater.")
+# else:
+#     print("c is a greater")
+
+
+#TO CHECK A MULTIPLE OF 7 OR NOT..........
+
+a = float(input("ENTER THE NUMBER: "))
+if (a%7 == 0):
+    print("YES")
+else:
+    print("NO")
